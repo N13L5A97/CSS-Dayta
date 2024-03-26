adc54162f22776b883bd8296188029b30df3a434
Niels Aling <niels.aling@hva.nl>
2024-03-26 14:17:26 +0100

    fix clock  flip on click with z-index

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-// fetch css day-ta
-const fetchData = async () => {
-  try {
-    const res = await fetch('https://cssday.nl/data.json');
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-};
 
-fetchData();
